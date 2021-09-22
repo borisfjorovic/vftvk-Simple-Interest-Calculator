44c2c42942efd18ae39676751b77d2932f8d2473
Boris Fjorović <bfjorovic@span.eu>
2021-09-22 16:35:43 +0200

    Added mark for the numbers in the output highlighted in yellow

--- script.js
+++ script.js
@@ -6,7 +6,10 @@ function compute(){
     var interest = principal * years * rate / 100;
     var year  = new Date().getFullYear() + years;
 
-    document.getElementById("result").innerHTML = "Interest : If you deposit " + principal + ",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
+    document.getElementById("result").innerHTML = "Interest: If you deposit <mark>" + principal + "</mark>" +
+    ",\<br\>at an interest rate of <mark>" + rate + "</mark>" +
+    "%\<br\>You will receive an amount of <mark>" + interest +  "</mark>" +
+     ",\<br\>in the year <mark>" + year + "</mark>"; 
 }
 //update slider value to the span rate
 function updateRate(){
